f50a2c36761b609b78f5629276828a48e112c8ad
anthony morris <anthonymorris@anthonys-MacBook-Pro.local>
2020-04-24 13:55:34 +0100

    changed logo name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
         </NavWrapper>
         <BuisnessLogo>
           TOWIDOMO <br />
-          .TECH
+          .DEV
         </BuisnessLogo>
       </TopBar>
     </>
