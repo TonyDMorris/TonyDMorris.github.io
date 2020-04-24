6971f30f74c16c277fd2b134019b23b24c0f3741
anthony morris <anthonymorris@anthonys-MacBook-Pro.local>
2020-04-24 13:50:24 +0100

    Updates

---
+++ precache-manifest.c3cf5ffd77d07768d9f2b2d65f69a1e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3c6ca4833cc2f6db9eafc2860c0fb5",
+    "url": "/towidomo/d179ddd821040f2eb41f.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/towidomo/d179ddd821040f2eb41f.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda9b89c9655f0b2dcc3b8c4fb314619",
+    "url": "/towidomo/index.html"
+  },
+  {
+    "revision": "0a818a5e2aa86e10bf77",
+    "url": "/towidomo/static/css/2.c83c8d54.chunk.css"
+  },
+  {
+    "revision": "1ab3f719be9dc42ae012",
+    "url": "/towidomo/static/css/main.dff13ae3.chunk.css"
+  },
+  {
+    "revision": "0a818a5e2aa86e10bf77",
+    "url": "/towidomo/static/js/2.8a950f45.chunk.js"
+  },
+  {
+    "revision": "27b2f875eb34a53d114fa1dad78a90da",
+    "url": "/towidomo/static/js/2.8a950f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab3f719be9dc42ae012",
+    "url": "/towidomo/static/js/main.16502fda.chunk.js"
+  },
+  {
+    "revision": "480df6686e3224a79c1e",
+    "url": "/towidomo/static/js/runtime-main.b39f589f.js"
+  },
+  {
+    "revision": "131dbc72ef3af778e86d34efb1fc8972",
+    "url": "/towidomo/static/media/JetBrainsMono-ExtraBold.131dbc72.ttf"
+  }
+]);
